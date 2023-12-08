b194c2f4eff71117c546391c8b68a5810cb8f9a5
CTCycle <thomas.virdis89@gmail.com>
2023-12-08 16:48:56 +0100

    added placeholders for k fold training

--- FAIRS.py
+++ FAIRS.py
@@ -23,6 +23,10 @@ operations_menu = {'1' : 'Data analysis',
                    '3' : 'FAIRS training: NumberMatrix Model (NMM)',
                    '4' : 'Predict next extraction',                                                 
                    '5' : 'Exit and close'}
+model_menu = {'1' : 'Standard training',
+              '2' : 'K-fold training',                                                          
+              '3' : 'Exit and close'}
+
 
 while True:
     print('------------------------------------------------------------------------')    
@@ -30,13 +34,31 @@ while True:
     print()     
     if op_sel == 1:
         import modules.timeseries_analysis
-        del sys.modules['modules.CCM_training'] 
+        del sys.modules['modules.timeseries_analysis'] 
     elif op_sel == 2:
-        import modules.CCM_training
-        del sys.modules['modules.CCM_training']
+        while True:    
+            mod_sel = user_operations.menu_selection(model_menu)
+            print()
+            if mod_sel == 1:
+                import modules.CCM_training
+                del sys.modules['modules.CCM_training']
+            elif mod_sel == 2: 
+                import modules.CCM_kfold_training
+                del sys.modules['modules.CCM_kfold_training']
+            elif mod_sel == 3: 
+                break
     elif op_sel == 3:
-        import modules.PMM_training
-        del sys.modules['modules.PMM_training']           
+        while True:    
+            mod_sel = user_operations.menu_selection(model_menu)
+            print()
+            if mod_sel == 1:
+                import modules.NMM_training
+                del sys.modules['modules.NMM_training']
+            elif mod_sel == 2: 
+                import modules.NMM_kfold_training
+                del sys.modules['modules.NMM_kfold_training']
+            elif mod_sel == 3: 
+                break           
     elif op_sel == 4:
         import modules.FAIRS_forecast
         del sys.modules['modules.FAIRS_forecast']
