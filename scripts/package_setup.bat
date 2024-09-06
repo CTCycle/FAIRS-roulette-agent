d54b99208fe55333dbc8fa223e8c4cf837441e2b
CTCycle <thomas.virdis89@gmail.com>
2024-09-06 16:39:40 +0200

    install through pyproject.toml now

--- scripts/package_setup.bat
+++ scripts/package_setup.bat
@@ -1,7 +1,7 @@
 @echo off
-rem Use this script to install packages in developer mode
+rem Use this script to create a new environment called "FAIRS"
 
-call conda activate FAIRS && cd .. && pip install -e .
+call conda activate FAIRS && cd .. && pip install -e . --use-pep517
 if errorlevel 1 (
     echo Failed to install the package in editable mode
     goto :eof
