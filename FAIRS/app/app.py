c487b45b179c7fcb47bd99770a05aac0f4d1b2c3
CTCycle <thomas.virdis89@gmail.com>
2025-08-16 19:23:30 +0200

    added actions to delete and export all data from database

--- FAIRS/app/app.py
+++ FAIRS/app/app.py
@@ -1,26 +1,20 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from qt_material import apply_stylesheet
 
 # [SETTING WARNINGS]
 import warnings
 warnings.simplefilter(action='ignore', category=Warning)
 
 # [IMPORT CUSTOM MODULES]
-from FAIRS.app.client.window import MainWindow
+from FAIRS.app.client.window import apply_style, MainWindow
 from FAIRS.app.constants import UI_PATH
-from FAIRS.app.logger import logger
 
 
 # [RUN MAIN]
 ###############################################################################
 if __name__ == "__main__":  
     app = QApplication(sys.argv) 
-
-    # setup stylesheet
-    extra = {'density_scale': '-1'}
-    apply_stylesheet(app, theme='dark_yellow.xml', extra=extra)
-
+    app = apply_style(app)  
     main_window = MainWindow(UI_PATH)   
     main_window.show()
     sys.exit(app.exec())
