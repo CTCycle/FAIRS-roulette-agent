161b1f134ea201e576e8b04f156d875d24add07b
CTCycle <thomas.virdis89@gmail.com>
2025-08-28 18:15:44 +0200

    added hitning to some parts of window

--- FAIRS/app/client/window.py
+++ FAIRS/app/client/window.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
-
-from typing import Any, Callable, cast
+from typing import Any, Callable, List, cast
 
 import pandas as pd
 from FAIRS.app.variables import EnvironmentVariables
